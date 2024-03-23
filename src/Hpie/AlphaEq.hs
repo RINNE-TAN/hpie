daaea75510f2cb676fd99895e4d6dd106c3ac1fd
kamome <2038975825@qq.com>
2024-03-23 22:40:25 +0800

    refactor alpha eq

--- src/Hpie/AlphaEq.hs
+++ src/Hpie/AlphaEq.hs
@@ -1,13 +1,9 @@
 module Hpie.AlphaEq where
 
 import Data.Foldable (traverse_)
-import Hpie.Env (TcMonad, throwE)
+import Hpie.Env (TcMonad)
+import qualified Hpie.Env as Env
 import Hpie.Types
-
-alphaEq :: Term -> Term -> TcMonad ()
-alphaEq t1 t2 = case runAlpha (eq t1 t2) [] [] 0 of
-  Left e -> throwE e
-  Right _ -> return ()
 
 newtype Alpha a = Alpha
   { runAlpha ::
@@ -22,8 +18,15 @@ instance Functor Alpha where
 
 instance Applicative Alpha where
   pure x = Alpha (\_ _ _ -> Right x)
-  Alpha fun <*> Alpha arg =
-    Alpha (\l r i -> fun l r i <*> arg l r i)
+  (<*>) f a = f >>= (<$> a)
+
+instance Monad Alpha where
+  (>>=) alphaA f =
+    Alpha
+      ( \l r i -> case runAlpha alphaA l r i of
+          Left err -> Left err
+          Right a -> runAlpha (f a) l r i
+      )
 
 with :: Symbol -> Symbol -> Alpha a -> Alpha a
 with x y (Alpha act) =
@@ -43,10 +46,15 @@ rawEq l r
 listEq :: [a] -> [a] -> (a -> a -> Alpha ()) -> Alpha ()
 listEq llist rlist f = rawEq (length llist) (length rlist) *> traverse_ (uncurry f) (zip llist rlist)
 
-eq :: Term -> Term -> Alpha ()
-eq left right =
-  case (left, right) of
-    (Var x, Var y) ->
+alpha2tc :: Alpha a -> TcMonad a
+alpha2tc alpha = case runAlpha alpha [] [] 0 of
+  Left e -> Env.throwE e
+  Right v -> return v
+
+alphaEq :: Term -> Term -> TcMonad ()
+alphaEq = \t1 t2 -> alpha2tc (go t1 t2)
+  where
+    go (Var x) (Var y) =
       Alpha
         ( \l r _ -> case (lookup x l, lookup y r) of
             (Nothing, Nothing)
@@ -57,31 +65,31 @@ eq left right =
               | otherwise -> Left $ AlphaNotEq x y
             _ -> Left $ AlphaNotEq x y
         )
-    (Pi x1 a1 b1, Pi x2 a2 b2) ->
-      eq a1 a2 *> with x1 x2 (eq b1 b2)
-    (Arrow a1 b1, Arrow a2 b2) -> eq a1 b1 *> eq a2 b2
-    (Lam x1 t1, Lam x2 t2) ->
-      with x1 x2 (eq t1 t2)
-    (Rec x1 t1, Rec x2 t2) -> with x1 x2 (eq t1 t2)
-    (App f1 arg1, App f2 arg2) -> eq f1 f2 *> eq arg1 arg2
-    (Sigma x1 a1 b1, Sigma x2 a2 b2) ->
-      eq a1 a2 *> with x1 x2 (eq b1 b2)
-    (Pair a1 b1, Pair a2 b2) -> eq a1 a2 *> eq b1 b2
-    (Prod l1 r1, Prod l2 r2) ->
-      eq l1 l2 *> eq r1 r2
-    (First p1, First p2) -> eq p1 p2
-    (Second p1, Second p2) -> eq p1 p2
-    (TyCon x1 args1, TyCon x2 args2) ->
+    go (Pi x1 a1 b1) (Pi x2 a2 b2) =
+      go a1 a2 *> with x1 x2 (go b1 b2)
+    go (Arrow a1 b1) (Arrow a2 b2) = go a1 a2 *> go b1 b2
+    go (Lam x1 term1) (Lam x2 term2) =
+      with x1 x2 (go term1 term2)
+    go (Rec x1 term1) (Rec x2 term2) = with x1 x2 (go term1 term2)
+    go (App f1 arg1) (App f2 arg2) = go f1 f2 *> go arg1 arg2
+    go (Sigma x1 a1 b1) (Sigma x2 a2 b2) =
+      go a1 a2 *> with x1 x2 (go b1 b2)
+    go (Pair a1 b1) (Pair a2 b2) = go a1 a2 *> go b1 b2
+    go (Prod l1 r1) (Prod l2 r2) =
+      go l1 l2 *> go r1 r2
+    go (First p1) (First p2) = go p1 p2
+    go (Second p1) (Second p2) = go p1 p2
+    go (TyCon x1 args1) (TyCon x2 args2) =
       rawEq x1 x2
-        *> listEq args1 args2 eq
-    (DataCon x1 args1, DataCon x2 args2) ->
+        *> listEq args1 args2 go
+    go (DataCon x1 args1) (DataCon x2 args2) =
       rawEq x1 x2
-        *> listEq args1 args2 eq
-    (Match term1 case1, Match term2 case2) ->
-      eq term1 term2
+        *> listEq args1 args2 go
+    go (Match term1 case1) (Match term2 case2) =
+      go term1 term2
         *> listEq
           case1
           case2
-          (\(Case pat1 c1) (Case pat2 c2) -> rawEq pat1 pat2 *> eq c1 c2)
-    (U, U) -> yes
-    (l, r) -> no l r
+          (\(Case pat1 c1) (Case pat2 c2) -> rawEq pat1 pat2 *> go c1 c2)
+    go U U = yes
+    go l r = no l r
